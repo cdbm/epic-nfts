741994b0e3eb5686eecf24a14539df8d9d2f40bb
cdbm <cdbm@cin.ufpe.br>
2021-12-12 14:03:32 -0300

    encoded json data and random nft generator onchain

--- scripts/run.js
+++ scripts/run.js
@@ -2,12 +2,14 @@ const main = async () => {
     const nftContractFactory = await hre.ethers.getContractFactory('MyEpicNFT');
     const nftContract = await nftContractFactory.deploy();
     await nftContract.deployed();
-
-    console.log("Contract deployed to: ", nftContract.address);
+    console.log("Contract deployed to:", nftContract.address);
 
     let txn = await nftContract.makeAnEpicNFT()
+    await txn.wait()
 
-    await txn.wait();
+    txn = await nftContract.makeAnEpicNFT()
+    await txn.wait()
+
 };
 
 const runMain = async () => {
